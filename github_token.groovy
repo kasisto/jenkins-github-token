8a14e52b19a4b1ef819e96af4e54bb3bb7414574
Sean Walsh <seanwalsh@Seans-Air.fios-router.home>
2020-02-13 19:59:21 -0500

    get access token

--- github_token.groovy
+++ github_token.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 import org.jenkinsci.plugins.GithubAccessTokenProperty
 import hudson.model.User
-import hudson.model.*
-import jenkins.security.*
+
 
 def github_token() {
     userId = 'gizabutler' //build.getCause(Cause.UserIdCause).getUserId()
